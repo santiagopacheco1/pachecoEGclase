e560bada196d7beeb0851ce90ff9440c5379452c
santiago Pacheco <santi.pacheco100100@gmail.com>
2021-06-16 19:29:35 -0300

    cambios 2

---
+++ practica 5/ejercicio1.php
@@ -0,0 +1,15 @@
+<?php $destinatario = "xx@xx.com ";
+$asunto = "Prueba";
+$cuerpo = ' 
+<html> 
+<head> 
+<title>Envio de mail</title> 
+</head>
+<body>
+<h1>Hola!</h1>
+<p>
+<b>Esto es una prueba
+</b>. xxxxxxxxxxxxxxxxxxxxxxxxxxxx</p> 
+</body>
+</html> ';
+?>
